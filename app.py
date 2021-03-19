9d47981bd50db9a0b125cc4da6b9d700dc11049a
Fabio Kfouri <fabiokfouri@suzano.com.br>
2021-03-19 18:35:58 -0300

    usando coverage

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 2) + fibonacci(n - 1)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
--- test_app.py
+++ test_app.py
@@ -5,5 +5,11 @@ class TestFibonacci(unittest.TestCase):
     def test_fibonnacci_1(self):
         self.assertEqual(fibonacci(1), 1)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_fibonnacci_10(self):
+        self.assertEqual(fibonacci(10), 89)
+
+    def test_fibonnacci_30(self):
+        self.assertEqual(fibonacci(30), 1346269)
+
+#if __name__ == '__main__':
+#    unittest.main()
